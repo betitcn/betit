c43f574333b093274c144dbe78e5301edddef6c9
summit <summit.qiu@gmail.com>
2012-12-27 20:10:06 +0800

    andriod push

--- capi/source/cp_bindandriod.php
+++ capi/source/cp_bindandriod.php
@@ -4,9 +4,17 @@ if(!defined('IN_UCHOME')) {
 	exit('Access Denied');
 }
 
-updatetable('space', array('isandriod'=>1), array('uid'=>$_SGLOBAL['supe_uid']));
+$bind = empty($_REQUEST['bind'])?1:0;
 
-capi_showmessage_by_data('rest_success', 0);
+if ($bind){
+	updatetable('space', array('isandriod'=>1), array('uid'=>$_SGLOBAL['supe_uid']));
+
+	capi_showmessage_by_data('rest_success', 0);
+}else{
+	updatetable('space', array('isandriod'=>0), array('uid'=>$_SGLOBAL['supe_uid']));
+
+	capi_showmessage_by_data('rest_success', 0);
+}
 
 
 ?>
