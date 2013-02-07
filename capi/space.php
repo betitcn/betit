1eb7e8ea81ecee9bbf6abf3c8686f7734346386f
zhongwei0752 <zhongwei0752@gmail.com>
2013-02-08 01:01:27 +0800

    接口修复

--- capi/space.php
+++ capi/space.php
@@ -28,7 +28,7 @@ if($_SCONFIG['allowrewrite'] && isset($_REQUEST['rewrite'])) {
 }
 
 //允许动作
-$dos = array('feed', 'doing', 'mood', 'blog', 'album', 'thread', 'mtag', 'friend', 'wall', 'tag', 'notice', 'share', 'topic', 'home', 'pm', 'event', 'poll', 'top', 'info', 'videophoto', /* chenler add */ 'quiz','stat', 'isonline', 'task');
+$dos = array('feed', 'doing', 'mood', 'blog', 'album', 'thread', 'mtag', 'friend', 'wall', 'tag', 'notice', 'share', 'topic', 'home', 'pm', 'event', 'poll', 'top', 'info', 'videophoto', /* chenler add */ 'quiz','stat', 'isonline', 'task','ajax');
 
 //获取变量
 $isinvite = 0;
