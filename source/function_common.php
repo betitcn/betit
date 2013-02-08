193513727f15034f6cf921854e1ebd9d0bcf1f2b
zhongwei0752 <zhongwei0752@gmail.com>
2013-02-09 02:02:05 +0800

    相册修复

--- source/function_common.php
+++ source/function_common.php
@@ -1303,10 +1303,9 @@ function pic_get($filepath, $thumb, $remote, $return_thumb=1) {
 			
 			if($remote) {
 				$url .= '!210x210';
+			}else{
+				$url .= '.thumb.jpg';
 			}
-			//else{
-			//	$url .= ' ';
-			//}
 		}
 		if($remote) {
 			$url = $_SCONFIG['ftpurl'].$url;
