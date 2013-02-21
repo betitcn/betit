a97bedcdea17c3d750b84b303f557a894c59a512
zhongwei0752 <zhongwei0752@gmail.com>
2013-02-22 00:19:43 +0800

    发布竞猜提示修复

--- source/space_ajax.php
+++ source/space_ajax.php
@@ -8,7 +8,7 @@ if($keywords){
 			}
 		}
 $query = $_SGLOBAL['db']->query("SELECT p.*,pf.* FROM  ".tname('quiz')." p USE INDEX (voternum)  LEFT JOIN ".tname('feed')." pf ON pf.id=p.quizid
-     where `subject` like '$keywords%' AND p.uid IN('".implode("','",  $searcharr)."') AND p.id!=1 limit 0,10;"); 
+     where p.subject like '%$keywords%' AND p.uid IN('".implode("','",  $searcharr)."') AND p.id!=1 limit 0,10;"); 
 	
 while($value = $_SGLOBAL['db']->fetch_array($query))
  { 
