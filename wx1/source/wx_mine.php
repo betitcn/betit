e143d52e134dc4cc9b4b69d1290a4f71009edbf4
zhongwei0752 <zhongwei0752@gmail.com>
2013-03-18 22:46:20 +0800

    腾讯微博

--- wx1/source/wx_mine.php
+++ wx1/source/wx_mine.php
@@ -25,7 +25,9 @@ if ($_SGLOBAL['db']->fetch_array($query)){
 	
 
 	// 同步登陆
-	$sinauid=$_GET['uid'];
+	$sinauid=$_GET['sinauid'];
+	$qquid=$_GET['qquid'];
+	if($sinauid){
 	$jsonurl = "http://www.betit.cn/capi/connect.php?site=weibo&sinauid=$sinauid";
 	$json = file_get_contents($jsonurl,0,null,null);
 	$os = mobile_user_agent_switch();
@@ -44,8 +46,28 @@ if ($_SGLOBAL['db']->fetch_array($query)){
 	updatetable('space', array('wxkey'=>$_POST['wxkey'], 'device'=>$device), array('uid'=>$json_output->data->space->uid));
 	
 	echo "<script>localStorage.removeItem('auth');localStorage.setItem('auth','".$json_output->data->m_auth."');</script>";
+	}
+	if($qquid){
+		$jsonurl = "http://www.betit.cn/capi/connect.php?site=qq&qqopenid=$qquid";
+	$json = file_get_contents($jsonurl,0,null,null);
+	$os = mobile_user_agent_switch();
+	$json_output = json_decode($json);
+	$loginusername=$json_output->data->space->name;
+	$credit=$json_output->data->space->credit;
+	$experience=$json_output->data->space->experience;
+	$avatar=$json_output->data->space->avatar;
+	$note=$json_output->data->space->note;
+	$friendnum=$json_output->data->space->friendnum;
+	$quiznum=$json_output->data->space->quiznum;
+	//$avatar = capi_avatar($value["uid"]);
+	$device = json_encode(array("os"=>$os, "auth"=>$json_output->data->m_auth));
+	// bind
 	
+	updatetable('space', array('wxkey'=>$_POST['wxkey'], 'device'=>$device), array('uid'=>$json_output->data->space->uid));
+	
+	echo "<script>localStorage.removeItem('auth');localStorage.setItem('auth','".$json_output->data->m_auth."');</script>";
 
+	}
 	
 
 	// showmessage('do_success', 'wx.php?do=feed&wxkey='.$_POST['wxkey'], 0);
