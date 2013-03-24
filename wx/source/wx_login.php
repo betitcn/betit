6762d3e9e3c3320451189de6fba5c1324ed83afd
zhongwei0752 <zhongwei0752@gmail.com>
2013-03-25 03:45:38 +0800

    wxkey

--- wx/source/wx_login.php
+++ wx/source/wx_login.php
@@ -1,9 +1,10 @@
 <?php
 require_once 'wx_common.php';	
+require_once CONNECT_ROOT."/common/jtee.inc.php";
  $m_auth = getAuth();
 
 $wxkey=$_GET['wxkey'];
-wxshowmessage($wxkey);
+setSession($wxkey,$wxkey);
 	
 
 	include_once template("./wx/template/login");
