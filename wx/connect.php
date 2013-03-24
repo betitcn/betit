b7f5d570a5d04d10136fda2edfea2e8f8184eb30
zhongwei0752 <zhongwei0752@gmail.com>
2013-03-25 03:34:40 +0800

    wxkey

--- wx/connect.php
+++ wx/connect.php
@@ -1,11 +1,12 @@
 <?php
-$wxkey=$_GET['wxkey'];
+
 $site = empty($_GET['site'])?"weibo":$_GET['site'];
 $ac = empty($_GET['ac'])?'':$_GET['ac'];
 
 if ($site == "weibo")
 {
 	if ($ac=="callback"){
+		$wxkey=$_GET['wxkey'];
 		require 'weibo/callback.php';
 	}else{
 		require 'weibo/index.php';
