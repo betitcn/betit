107e334391aaeb9137c52185dbd51e90c130915f
zhongwei0752 <zhongwei0752@gmail.com>
2013-03-25 03:25:49 +0800

    wxkey

--- wx/connect.php
+++ wx/connect.php
@@ -1,5 +1,7 @@
 <?php
-
+require_once 'wx_common.php';
+$wxkey=$_GET['wxkey'];
+wxshowmessage($wxkey);
 $site = empty($_GET['site'])?"weibo":$_GET['site'];
 $ac = empty($_GET['ac'])?'':$_GET['ac'];
 
