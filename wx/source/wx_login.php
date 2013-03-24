37d239c4fc317f7fb1d7c701deacc5c099336ad8
zhongwei0752 <zhongwei0752@gmail.com>
2013-03-25 04:14:33 +0800

    wxkey

--- wx/source/wx_login.php
+++ wx/source/wx_login.php
@@ -1,24 +1,9 @@
 <?php
-require_once 'wx_common.php';	
+require_once 'wx_common.php';
+require_once 'wx_wxkey.php';	
 require_once '../common.php';
 
-function setSession($uid, $username) {
-	$setarr = array(
-		'uid' => $uid,
-		'username' => $username,
-		'password' => md5($uid."|".time())//本地密码随机生成
-	);
-	//在线session
-	include_once(S_ROOT.'./source/function_space.php');
-	$setarr = insertsession($setarr);
-	$auth =  authcode("$setarr[password]\t$uid", 'ENCODE');
-	//设置cookie
-	ssetcookie('auth', $auth, 31536000);//原数据2592000
-	ssetcookie('wxkey', $username, 31536000);
-	ssetcookie('_refer', '');
 
-	return $auth;
-}
  $m_auth = getAuth();
 
 $wxkey=$_GET['wxkey'];
