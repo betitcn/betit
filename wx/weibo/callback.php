f5360098c10d6473f87ae57c4b91e2e7c8373f1a
zhongwei0752 <zhongwei0752@gmail.com>
2013-03-25 03:36:34 +0800

    wxkey

--- wx/weibo/callback.php
+++ wx/weibo/callback.php
@@ -33,10 +33,8 @@ if ($token) {
 		uc_user_synlogin($rst['uid']);
 		setSession($user[0],$user[1]);
 		$sinauid=$uid_get['uid'];
-		include_once( 'connect.php' );
 		
-		wxshowmessage($wxkey);
-		//wxshowmessage('do_success',"wx.php?do=mine&sinauid=$sinauid");
+		wxshowmessage('do_success',"wx.php?do=mine&sinauid=$sinauid");
 	}else{
 		 $c = new SaeTClientV2( WB_AKEY , WB_SKEY , $_SESSION['token']['access_token'] );
 		 $profile = $c->show_user_by_id($uid_get['uid']);
