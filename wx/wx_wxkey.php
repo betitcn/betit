66205b9e33bc99682df038876ec956dfafda516a
zhongwei0752 <zhongwei0752@gmail.com>
2013-03-25 04:23:17 +0800

    wxkey

--- wx/wx_wxkey.php
+++ wx/wx_wxkey.php
@@ -1,19 +1,9 @@
 <?php
-function setSession($uid, $username) {
-	$setarr = array(
-		'uid' => $uid,
-		'username' => $username,
-		'password' => md5($uid."|".time())//本地密码随机生成
-	);
-	//在线session
-	include_once(S_ROOT.'./source/function_space.php');
-	$setarr = insertsession($setarr);
-	$auth =  authcode("$setarr[password]\t$uid", 'ENCODE');
-	//设置cookie
-	ssetcookie('auth', $auth, 31536000);//原数据2592000
-	ssetcookie('wxkey', $username, 31536000);
-	ssetcookie('_refer', '');
+function setSession($wxkey) {
 
-	return $auth;
+	ssetcookie('wxkey', $username, 31536000);
+
+
+
 }
 ?>
