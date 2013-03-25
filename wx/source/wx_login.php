ec9b0692ae529dc423e6eb923b04ebaa4312c338
zhongwei0752 <zhongwei0752@gmail.com>
2013-03-25 14:29:50 +0800

    wxkey

--- wx/source/wx_login.php
+++ wx/source/wx_login.php
@@ -1,7 +1,10 @@
 <?php
-require_once 'wx_common.php';
-	
+include_once( 'weibo/config.php' );
 require_once '../common.php';
+require_once 'wx_common.php';
+include_once( CONNECT_ROOT.'/saetv2.ex.class.php' );
+require_once CONNECT_ROOT."/common/jtee.inc.php";
+require_once CONNECT_ROOT."/common/siteUserRegister.class.php";
 
 
  $m_auth = getAuth();
