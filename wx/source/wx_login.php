f5a684ac5cb3d9b8cc3c0ce029daee447ae8f257
zhongwei0752 <zhongwei0752@gmail.com>
2013-03-25 13:48:55 +0800

    wxkey

--- wx/source/wx_login.php
+++ wx/source/wx_login.php
@@ -7,7 +7,7 @@ require_once '../common.php';
  $m_auth = getAuth();
 
 $wxkey=$_GET['wxkey'];
-ssetcookie('wxkey', $wxkey, 31536000);
+ssetcookie('wxkey', '123', 31536000);
 
 	include_once template("./wx/template/login");
 ?>
