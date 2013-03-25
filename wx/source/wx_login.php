1a30f587971e6e634faf6cb19d28d2559d00afe3
zhongwei0752 <zhongwei0752@gmail.com>
2013-03-25 14:56:24 +0800

    wxkey

--- wx/source/wx_login.php
+++ wx/source/wx_login.php
@@ -1,16 +1,13 @@
 <?php
-include_once( 'weibo/config.php' );
 require_once '../common.php';
 require_once 'wx_common.php';
-include_once( CONNECT_ROOT.'/saetv2.ex.class.php' );
-require_once CONNECT_ROOT."/common/jtee.inc.php";
-require_once CONNECT_ROOT."/common/siteUserRegister.class.php";
+
 
 
  $m_auth = getAuth();
-
+session_start();  
 $wxkey=$_GET['wxkey'];
-setcookie('wxkey', "123");
+   $_SESSION['wxkey'] = $wxkey;
 
 	include_once template("./wx/template/login");
 ?>
