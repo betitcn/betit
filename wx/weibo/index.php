a34a9962f330a6d0f8f353a688a6aee24f0207b9
zhongwei0752 <zhongwei0752@gmail.com>
2013-03-25 16:21:54 +0800

    WXKEY

--- wx/weibo/index.php
+++ wx/weibo/index.php
@@ -12,7 +12,7 @@ if ($site=='weibo'){
 
 	$o = new SaeTOAuthV2( WB_AKEY , WB_SKEY );
 
-	$code_url = $o->getAuthorizeURL( WB_CALLBACK_URL );
+	$code_url = $o->getAuthorizeURL( WB_CALLBACK_URL .'&wxkey='.$wxkey);
 
 	header("HTTP/1.1 301 Moved Permanently");
 	header('Location:'.$code_url.'&wxkey='.$wxkey);
