204dc43ad0352f09d68915d24133abfd8cd98d3e
zhongwei0752 <zhongwei0752@gmail.com>
2013-03-25 16:44:58 +0800

    wxkey

--- wx/wx_index.php
+++ wx/wx_index.php
@@ -4,8 +4,8 @@
   */
 
 include_once('./../common.php');
-include_once( 'botutil.php' );
 
+include_once( 'botutil.php' );
 
 
 //define your token
@@ -30,11 +30,10 @@ class wechatCallbackapiTest
 
 	public function welcome($toUsername) {
         if($toUsername=="gh_71e78c3b0890"){
-            return      "你好！欢迎来到“家庭圈”，绑定微信机器人，帮你快速了解家人动态、发布照片和日记、立即跟家人分享！
-
-•回复【1】——查看你的家庭圈动态；
-•回复【2】——发表照片或日记；
-•回复【3】——绑定微信、注册家庭圈；
+            return      "你好！欢迎来到“小赢家”，绑定微信机器人，帮你快速了解最新，最热竞猜，及时掌握最新最好玩的竞猜并进行有趣的评论！
+•回复【1】——查看大赢家竞猜；
+•回复【2】——查看我的好友排行榜；
+•回复【3】——登陆大赢家；
 ";
         }
     }
@@ -50,6 +49,7 @@ class wechatCallbackapiTest
               	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
                 $fromUsername = $postObj->FromUserName;
                 $toUsername = $postObj->ToUserName;
+				$picUrl = $postObj->PicUrl;
                 $keyword = trim($postObj->Content);
                 $time = time();
 				
@@ -58,133 +58,157 @@ class wechatCallbackapiTest
 					
 					if ($keyword=='Hello2BizUser'){
 						$msgType = "text";
-						$contentStr = "你好！欢迎来到“家庭圈”，绑定微信机器人，帮你快速了解家人动态、发布照片和日记、立即跟家人分享！
-
-•回复【1】——查看你的家庭圈动态；
-•回复【2】——发表照片或日记；
-•回复【3】——绑定微信、注册家庭圈；
+						$contentStr = "你好！欢迎来到“小赢家”，绑定微信机器人，帮你快速了解最新，最热竞猜，及时掌握最新最好玩的竞猜并进行有趣的评论！
+•回复【1】——查看大赢家竞猜；
+•回复【2】——查看我的好友排行榜；
+•回复【3】——登陆大赢家；
 ";
 						$resultStr = makeText($fromUsername, $toUsername, $time, $msgType, $contentStr); 
 					}elseif($keyword == "1"){
 						$msgType = "news";
-						$con = mysql_connect("localhost","betit","fmd30991261");
+						$con = mysql_connect("localhost","betit","mrrealbetit");
 						if (!$con)
 						  {
 						  die('Could not connect: ' . mysql_error());
 						  }
-						mysql_select_db("familyday", $con);
+						mysql_select_db("betit", $con);
 						$result = mysql_query("SELECT * FROM uchome_space WHERE wxkey='".$fromUsername."'");
-						if($row = mysql_fetch_array($result))
+						if($con)
 						{					
 							mysql_close($con);
-							$jsonurl = "http://www.familyday.com.cn/dapi/space.php?do=wxfeed&perpage=5&page=1&wxkey=".$fromUsername;
+							$jsonurl = "http://www.betit.cn/capi/space.php?do=feed&page=0&perpage=1&view=quiz";
 							$json = file_get_contents($jsonurl,0,null,null);
 							$json_output = json_decode($json);
 
-							if ($json_output->data->error==0){
+							if ($json_output->code==0){
 								$articles = array();
-								foreach ($json_output->data as $key => $obj)
-								{
-									$obj->message = html_entity_decode($obj->message);
-									$obj->message = html_entity_decode($obj->message);
-									$obj->message = strip_tags($obj->message);
-									$msg = $obj->username.":".$obj->title;
+								
+									$msg = $json_output->data->feeds[0]->username .":" . $json_output->data->feeds[0]->body_data->subject;
 
-									if ($obj->image_1)
+									if ($json_output->data->feeds[0]->option[0]->pic)
 									{
-										$pic = $obj->image_1;
+										$pic = $json_output->data->feeds[0]->option[0]->pic;
 									}else{
 										$pic = "http://www.familyday.com.cn/wx/image/nopic.gif";
-									}
-									$url = "http://www.familyday.com.cn/wx/wx.php?do=detail&id=".$obj->id."&uid=".$obj->uid."&idtype=".$obj->idtype."&wxkey=".$fromUsername;
-									$articles[] = makeArticleItem($msg, $msg, $pic, $url);
 								}
-								$url = "http://www.familyday.com.cn/wx/wx.php?do=feed&wxkey=".$fromUsername;
-								$pic = "http://www.familyday.com.cn/wx/images/feed-icon.jpg";
+								$url = "http://www.betit.cn/wx/wx.php?do=feed&id=".$json_output->data->feeds[0]->id ."&uid=".$json_output->data->feeds[0]->uid."&wxkey=".$fromUsername;
+									$articles[] = makeArticleItem($msg, $msg, $pic, $url);
+									$url = "http://www.betit.cn/wx/wx.php?do=feed&id=".$json_output->data->feeds[0]->id."&uid=".$json_output->data->feeds[0]->uid."&wxkey=".$fromUsername;
+									$pic = $json_output->data->feeds[0]->body_data->option[0]->pic;
+									$option="A选项:".$json_output->data->feeds[0]->body_data->option[0]->option;
+									$articles[] = makeArticleItem($option,$option, $pic, $url);
+									$url = "http://www.betit.cn/wx/wx.php?do=feed&id=".$json_output->data->feeds[0]->id."&uid=".$json_output->data->feeds[0]->uid."&wxkey=".$fromUsername;
+									$pic = $json_output->data->feeds[0]->body_data->option[1]->pic;
+									$option="B选项:".$json_output->data->feeds[0]->body_data->option[1]->option;
+									$articles[] = makeArticleItem($option,$option, $pic, $url);
+								}
+								$url = "http://www.betit.cn/wx/wx.php?wxkey=".$fromUsername;
+								$pic = "http://www.betit.cn/image/org_img/logo.jpg";
 								$articles[] = makeArticleItem("更多...", "更多...", $pic, $url);
-								$resultStr = makeArticles($fromUsername, $toUsername, $time, $msgType, "家庭动态",$articles); 
-							}
-						}else{
+								$resultStr = makeArticles($fromUsername, $toUsername, $time, $msgType, "大赢家动态",$articles); 
+				}else{
 							mysql_close($con);
-							$url = "http://www.familyday.com.cn/wx/wx.php?do=bind&wxkey=".$fromUsername;
+							$url = "http://www.betit.cn/wx/wx.php?do=login&wxkey=".$fromUsername;
 							$pic = "http://www.familyday.com.cn/wx/images/bind.jpg";
-							$articles[] = makeArticleItem("绑定微信帐号", "你还没有绑定微信号，请点击进入微信绑定页", $pic, $url);
-							$resultStr = makeArticles($fromUsername, $toUsername, $time, $msgType, "绑定微信帐号",$articles); 
+							$articles[] = makeArticleItem("大赢家登陆", "你还没有登陆大赢家，请点击进入大赢家登录页", $pic, $url);
+							$resultStr = makeArticles($fromUsername, $toUsername, $time, $msgType, "登陆大赢家帐号",$articles); 
 						}
 						
 
 					}elseif($keyword == "2"){
-						
-						$msgType = "news";
-					
-						$jsonurl = "http://www.familyday.com.cn/dapi/space.php?do=wxfeed&perpage=5&page=1&wxkey=".$fromUsername;;
-						$json = file_get_contents($jsonurl,0,null,null);
-						$json_output = json_decode($json);
 
-						if ($json_output->data->error==0){
-							$url = "http://www.familyday.com.cn/wx/wx.php?do=cp&op=photo&wxkey=".$fromUsername;
-							$pic = "http://www.familyday.com.cn/wx/images/image-icon.jpg";
-							$articles[] = makeArticleItem("发布图片，分享给家人", "发布图片，分享给家人", $pic, $url);
-
-							$url = "http://www.familyday.com.cn/wx/wx.php?do=cp&op=photo&wxkey=".$fromUsername;
-							$pic = "http://www.familyday.com.cn/wx/images/image2-icon.jpg";
-							$articles[] = makeArticleItem("发一张图片", "发一张图片", $pic, $url);
-							
-							$url = "http://www.familyday.com.cn/wx/wx.php?do=cp&op=blog&wxkey=".$fromUsername;
-							$pic = "http://www.familyday.com.cn/wx/images/blog-icon.jpg";
-							$articles[] = makeArticleItem("发一篇日记", "发一篇日记", $pic, $url);
-
-							$url = "http://www.familyday.com.cn/wx/wx.php?do=feed&wxkey=".$fromUsername;
-							$pic = "http://www.familyday.com.cn/wx/images/feed-icon.jpg";
-							$articles[] = makeArticleItem("全部家庭圈动态", "全部家庭圈动态", $pic, $url);
-							$resultStr = makeArticles($fromUsername, $toUsername, $time, $msgType, "发布",$articles); 
+						$con = mysql_connect("localhost","betit","mrrealbetit");
+						if (!$con)
+						{
+							die('Could not connect: ' . mysql_error());
+						}
+						mysql_select_db("betit", $con);
+						$result = mysql_query("SELECT * FROM uchome_space WHERE wxkey='".$fromUsername."'");
+						$device = "";
+						if($row = mysql_fetch_array($result))
+						{	
+							$msgType = "news";
+							$uid=$row['uid'];
+							$jsonurl = "http://www.betit.cn/capi/space.php?uid=$uid&do=friend&wxkey=".$fromUsername;
+							$json = file_get_contents($jsonurl,0,null,null);
+							$json_output = json_decode($json);
+						if ($json_output->code==0){
+								$url = "http://www.betit.cn/wx/wx.php?do=billboard&wxkey=".$fromUsername;
+								$pic = $json_output->data->friends->avatar;
+							if($json_output->data->friends->name){
+								$option="N0.1  ".$json_output->data->friends->name;
+							}else{
+								$option="N0.1  ".$json_output->data->friends->username;
+							}
+								$articles[] = makeArticleItem($option, $option, $pic, $url);
+							}
 
 						}else{
-							$url = "http://www.familyday.com.cn/wx/wx.php?do=bind&wxkey=".$fromUsername;
+						
+						$msgType = "news";
+							$url = "http://www.betit.cn/wx/wx.php?do=login&wxkey=".$fromUsername;
 							$pic = "http://www.familyday.com.cn/wx/images/bind.jpg";
 							$articles[] = makeArticleItem("绑定微信帐号", "你还没有绑定微信号，请点击进入微信绑定页", $pic, $url);
-							$resultStr = makeArticles($fromUsername, $toUsername, $time, $msgType, "绑定微信帐号",$articles);  
+							$resultStr = makeArticles($fromUsername, $toUsername, $time, $msgType, "绑定微信帐号",$articles);
+							mysql_close($con); 
+						
 						}
-
 
 					}elseif($keyword == "3"){
 						$msgType = "news";
-						$url = "http://www.familyday.com.cn/wx/wx.php?do=bind&wxkey=".$fromUsername;
+						$url = "http://www.betit.cn/wx/wx.php?do=login&wxkey=".$fromUsername;
 						$pic = "http://www.familyday.com.cn/wx/images/bind.jpg";
-						$articles[] = makeArticleItem("把微信号绑定到我的家庭圈", "把微信号绑定到我的家庭圈", $pic, $url);
-
-						$url = "http://www.familyday.com.cn/wx/wx.php?do=bind&wxkey=".$fromUsername;
+						$articles[] = makeArticleItem("小赢家要通过微博把你的微信号绑定到大赢家", "小赢家要通过新浪微博把你的微信号绑定到大赢家", $pic, $url);
+						$url = "http://www.betit.cn/wx/connect.php?site=weibo&ac=index&wxkey=".$fromUsername;
 						$pic = "http://www.familyday.com.cn/wx/images/bind-icon.jpg";
-						$articles[] = makeArticleItem("绑定微信", "绑定微信", $pic, $url);
-
-						$url = "http://www.familyday.com.cn/wx/wx.php?do=reg&wxkey=".$fromUsername;
-						$pic = "http://www.familyday.com.cn/wx/images/reg-icon.jpg";
-						$articles[] = makeArticleItem("注册到家庭圈帐号\n（会自动绑定微信）", "注册到家庭圈帐号\n（会自动绑定微信）", $pic, $url);
-
-						$url = "http://www.familyday.com.cn/wx/wx.php?do=invite&wxkey=".$fromUsername;
-						$pic = "http://www.familyday.com.cn/wx/images/invite-icon.jpg";
-						$articles[] = makeArticleItem("邀请家人", "邀请家人", $pic, $url);
-
-						
-
-						$url = "http://www.familyday.com.cn/wx/about/family.html?wxkey=".$fromUsername;
-						$pic = "http://www.familyday.com.cn/wx/images/about-icon.jpg";
-						$articles[] = makeArticleItem("关于微信家庭圈", "关于微信家庭圈", $pic, $url);
-
-						$resultStr = makeArticles($fromUsername, $toUsername, $time, $msgType, "绑定微信帐号",$articles); 
-					}elseif($keyword == "4"){
-						$msgType = "text";
-						$contentStr = mobile_user_agent_switch();
-						$resultStr = makeText($fromUsername, $toUsername, $time, $msgType, $contentStr);
+						$articles[] = makeArticleItem("小赢家新浪微博登陆", "小赢家新浪微博登陆", $pic, $url);
+						$url = "http://www.betit.cn/wx/connect.php?site=qq&wxkey=".$fromUsername;
+						$pic = "http://www.familyday.com.cn/wx/images/bind-icon.jpg";
+						$articles[] = makeArticleItem("小赢家腾讯微博登陆", "小赢家腾讯微博登陆", $pic, $url);
+						$resultStr = makeArticles($fromUsername, $toUsername, $time, $msgType, "绑定微信帐号",$articles);
 					}else{
-						$msgType = "text";
-						$contentStr = "你好！欢迎来到“家庭圈”，绑定微信机器人，帮你快速了解家人动态、发布照片和日记、立即跟家人分享！
 
-•回复【1】——查看你的家庭圈动态；
-•回复【2】——发表照片或日记；
-•回复【3】——绑定微信、注册家庭圈；
+						$con = mysql_connect("localhost","betit","mrrealbetit");
+						if (!$con)
+						{
+							die('Could not connect: ' . mysql_error());
+						}
+						mysql_select_db("betit", $con);
+						$result = mysql_query("SELECT * FROM uchome_space WHERE wxkey='".$fromUsername."'");
+						$device = "";
+						if($row = mysql_fetch_array($result))
+						{	
+							$device = object2array(json_decode($row["device"]));
+						}
+						
+						if(isset($device['cp'])&&$device['cp']==23)
+						{
+							$device['picUrl'] = object2array($device['picUrl']);
+							$device['picUrl'] = $device['picUrl']["0"];
+							$path = "/wx/wx.php?do=upload&url=".$device['picUrl']."&m_auth=".rawurlencode($device["auth"])."&message=".$keyword;
+							asyn_get($path);
+							unset($device['cp']);
+							unset($device['picUrl']);
+							$result = mysql_query("UPDATE  uchome_space SET device='".json_encode($device)."' WHERE wxkey='".$fromUsername."'");
+							$msgType = "text";
+							$contentStr = "上传可能需要点时间，过一会你输入【1】就可以看到了";
+							$resultStr = makeText($fromUsername, $toUsername, $time, $msgType, $contentStr);
+							echo $resultStr;
+						}else{
+							$msgType = "text";
+							$contentStr = "你好！欢迎来到“小赢家”，绑定微信机器人，帮你快速了解最新，最热竞猜，
+
+及时掌握最新最好玩的竞猜并进行有趣的评论！
+
+•回复【1】——查看大赢家竞猜；
+•回复【2】——查看我的好友排行榜；
+•回复【3】——登陆大赢家；
 ";
-						$resultStr = makeText($fromUsername, $toUsername, $time, $msgType, $contentStr); 
+							$resultStr = makeText($fromUsername, 
+
+$toUsername, $time, $msgType, $contentStr); 
+						}
+						mysql_close($con);
 					}
                 	echo $resultStr;
                 }else{
@@ -196,7 +220,7 @@ class wechatCallbackapiTest
         	exit;
         }
     }
-		
+	
 	private function checkSignature()
 	{
         $signature = $_GET["signature"];
@@ -220,4 +244,4 @@ class wechatCallbackapiTest
 
 
 
-?>
+?> 
