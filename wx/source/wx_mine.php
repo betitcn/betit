b7a319058a591fa8450003d7ee23f079455f7941
zhongwei0752 <zhongwei0752@gmail.com>
2013-03-27 01:08:22 +0800

    12

--- wx/source/wx_mine.php
+++ wx/source/wx_mine.php
@@ -66,7 +66,7 @@ if ($_SGLOBAL['db']->fetch_array($query)){
 	//$avatar = capi_avatar($value["uid"]);
 	$device = json_encode(array("os"=>$os, "auth"=>$json_output->data->m_auth));
 	// bind
-	$wxkey=$_POST["wxkey"];
+	$wxkey=$_GET["wxkey"];
 	updatetable('space', array('wxkey'=>'1'.$wxkey, 'device'=>$device), array('uid'=>$json_output->data->space->uid));
 	
 	echo "<script>localStorage.removeItem('auth');localStorage.setItem('auth','".$json_output->data->m_auth."');</script>";
