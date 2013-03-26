e9b526ff36b200b9deffefaa72e568294777833f
zhongwei0752 <zhongwei0752@gmail.com>
2013-03-26 21:17:15 +0800

    wx

--- wx/template/js/login.js
+++ wx/template/js/login.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 	
 		var wxkey=$.query.get('wxkey');
-		localStorage.setItem("wxkey","3312");
+		window.localStorage.setItem("wxkey",wxkey);
 
-        )}
+        })
 
        
