b2fde8d4c218977b930af1078712a5caa96dec57
zhongwei0752 <zhongwei0752@gmail.com>
2013-03-26 19:49:29 +0800

    wx

--- wx/weibo/index.php
+++ wx/weibo/index.php
@@ -19,19 +19,19 @@ if ($site=='weibo'){
 
 }elseif($site=='qq'){
 
-	//$callback = 'http://www.betit.cn/connect.php?site=qq';//»Øµ÷url
+	//$callback = 'http://www.betit.cn/connect.php?site=qq';//回调url
 
 	OAuth::init(QQ_AKEY, QQ_SKEY);
 	Tencent::$debug = QQ_DEBUG;
 
 	header('Content-Type: text/html; charset=utf-8');
 
-	if ($_GET['code']) {//ÒÑ»ñµÃcode
+	if ($_GET['code']) {//已获得code
         $code = $_GET['code'];
         $openid = $_GET['openid'];
         $openkey = $_GET['openkey'];
         //»ñÈ¡ÊÚÈ¨token
-        $code_url = OAuth::getAccessToken($code, QQ_CALLBACK_URL);
+        $code_url = OAuth::getAccessToken($code, QQ_CALLBACK_URL.'&wxkey='.$wxkey);
         $r = Http::request($code_url);
         parse_str($r, $out);
         //´æ´¢ÊÚÈ¨Êý¾Ý
@@ -46,30 +46,30 @@ if ($site=='weibo'){
             //ÑéÖ¤ÊÚÈ¨
             $r = OAuth::checkOAuthValid();
             if ($r) {
-                header('Location: ' . $code_url.'&wxkey='.$wxkey);//Ë¢ÐÂÒ³Ãæ
+                header('Location: ' . $code_url.'&wxkey='.$wxkey);//刷新页面
             } else {
-                exit('<h3>ÊÚÈ¨Ê§°Ü,ÇëÖØÊÔ</h3>');
+                exit('<h3>授权失败,请重试</h3>');
             }
         } else {
             exit($r);
         }
-    } else {//»ñÈ¡ÊÚÈ¨code
+    } else {//获取授权code
 
 		
 
-        if ($_GET['openid'] && $_GET['openkey']){//Ó¦ÓÃÆµµÀ
+        if ($_GET['openid'] && $_GET['openkey']){//应用频道
             $_SESSION['t_openid'] = $_GET['openid'];
             $_SESSION['t_openkey'] = $_GET['openkey'];
             //ÑéÖ¤ÊÚÈ¨
             $r = OAuth::checkOAuthValid();
             if ($r) {
-                header('Location: ' . QQ_CALLBACK_URL.'&wxkey='.$wxkey);//Ë¢ÐÂÒ³Ãæ
+                header('Location: ' . QQ_CALLBACK_URL.'&wxkey='.$wxkey);//刷新页面
             } else {
-                exit('<h3>ÊÚÈ¨Ê§°Ü,ÇëÖØÊÔ</h3>');
+                exit('<h3>授权失败,请重试</h3>');
             }
         } else{
 
-           $code_url = OAuth::getAuthorizeURL( QQ_CALLBACK_URL .'&wxkey='.$wxkey);	//!!! ±ØÐëÓë OAuth::getAccessToken($code, QQ_CALLBACK_URL); µÄµØÖ·Ò»ÖÂ
+           $code_url = OAuth::getAuthorizeURL( QQ_CALLBACK_URL .'&wxkey='.$wxkey);	//!!! 必须与 OAuth::getAccessToken($code, QQ_CALLBACK_URL); 的地址一致
             header('Location: ' . $code_url.'&wxkey='.$wxkey);
         }
     }
