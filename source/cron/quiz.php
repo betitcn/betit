7067e0bda6c604ab9cf62cf549b2adadb8d4578b
zhongwei0752 <623610577@qq.com>
2013-04-08 13:10:39 +0800

    11

--- source/cron/quiz.php
+++ source/cron/quiz.php
@@ -48,13 +48,13 @@ while($value = $_SGLOBAL['db']->fetch_array($query)) {
 	$note = cplang('note_quiz_exceed3', array("space.php?uid=$value[uid]&do=quiz&id=$value[quizid]", $value['subject'],"space.php?uid=$value2[uid]&do=quiz&id=$value[fquizid]"));
 	}
 	admin_notification_add($value['uid'], 'quizexceed', $note);
-	notification_add_push($value['uid'],  $note, 1);
+	//notification_add_push($value['uid'],  $note, 1);
 	$_SGLOBAL['db']->query("UPDATE ".tname('quiz')." SET hasexceed='1' WHERE quizid='$value[quizid]'");
 	  
 
 	//获取参与用户
 	$query3 = $_SGLOBAL['db']->query("SELECT uid, count(*)*$value[joincost] as credit FROM ".tname('quizuser')." WHERE quizid='$value[quizid]' group by uid");
-	runlog("qqqqqq","SELECT uid, count(*)*$value[joincost] as credit FROM ".tname('quizuser')." WHERE quizid='$value[quizid]' group by uid");
+	//runlog("qqqqqq","SELECT uid, count(*)*$value[joincost] as credit FROM ".tname('quizuser')." WHERE quizid='$value[quizid]' group by uid");
 	while($value3 = $_SGLOBAL['db']->fetch_array($query3)) {
 	
 		//返还积分
@@ -80,7 +80,7 @@ while($value = $_SGLOBAL['db']->fetch_array($query)) {
 		
 		$note = cplang('note_quiz_exceed2', array("space.php?uid=$value[uid]&do=quiz&id=$value[quizid]", $value['subject'],$value3['credit']));
 		admin_notification_add($value3['uid'], 'quiz', $note);
-		notification_add_push($value3['uid'],  $note, 1);
+		//notification_add_push($value3['uid'],  $note, 1);
 	}	
 	
 }
