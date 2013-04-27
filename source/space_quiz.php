ef27a45bede0c6918df20fb5a7710d9d83d96792
zhongwei0752 <623610577@qq.com>
2013-04-27 13:11:00 +0800

    showmessage("adfas");

--- source/space_quiz.php
+++ source/space_quiz.php
@@ -23,13 +23,14 @@ $clicks = empty($_SGLOBAL['click']['quizid'])?array():$_SGLOBAL['click']['quizid
 if($id) {
 	//读取日志
 	$query = $_SGLOBAL['db']->query("SELECT bf.*, b.* FROM ".tname('quiz')." b LEFT JOIN ".tname('quizfield')." bf ON bf.quizid=b.quizid WHERE b.quizid='$id' AND b.uid='$space[uid]'");
+	showmessage("adfas");
 	$quiz = $_SGLOBAL['db']->fetch_array($query);
-showmessage("adfas");
+
 	//日志不存在
 	if(empty($quiz)) {
 		showmessage('view_to_info_did_not_exist');
 	}
-	
+
 	//检查好友权限
 	if(!ckfriend($quiz['uid'], $quiz['friend'], $quiz['target_ids'])) {
 		//没有权限
