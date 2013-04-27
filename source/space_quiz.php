e0424cbd703374ca991d37518be0a2bd31f79d6d
zhongwei0752 <623610577@qq.com>
2013-04-27 13:36:12 +0800

    showmessage("网站维护中");

--- source/space_quiz.php
+++ source/space_quiz.php
@@ -130,9 +130,9 @@ if($id) {
 	} else {
 
 		$quiz['related'] = empty($quiz['related'])?array():unserialize($quiz['related']);
-		showmessage("网站维护中");
+		
 	}
-
+	showmessage("网站维护中");
 	//作者的其他最新日志
 	$otherlist = array();
 	$query = $_SGLOBAL['db']->query("SELECT * FROM ".tname('quiz')." WHERE uid='$space[uid]' and id!=1 ORDER BY dateline DESC LIMIT 0,6");
