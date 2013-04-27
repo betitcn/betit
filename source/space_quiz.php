cc472bc3e7da326c9211f6ef1cccef8a87636d3e
zhongwei0752 <623610577@qq.com>
2013-04-27 22:36:52 +0800

    44

--- source/space_quiz.php
+++ source/space_quiz.php
@@ -634,7 +634,7 @@ $value3=array();
 	}
 		$query4 = $_SGLOBAL['db']->query("SELECT body_data FROM ".tname('feed')." WHERE id='$value[id]' ORDER BY id");
 $value4=array();
-if($query4)	{			
+		
 	while( $value4 = $_SGLOBAL['db']->fetch_array($query4))
 	{
 		
@@ -644,7 +644,7 @@ if($query4)	{
 
 
 	}	
-	}
+	
 			if($value['credit'] && $value['percredit'] && $value['credit'] < $value['percredit']) {
 				$value['percredit'] = $value['credit'];
 			}
